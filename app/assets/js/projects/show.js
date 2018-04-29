a3ccef5356f4e2b739bf325ac4c41ecc36b293d5
Steven Li <stevenli1141@gmail.com>
2018-04-29 19:42:45 -0400

    Show modals instead of direct link

--- app/assets/js/projects/show.js
+++ app/assets/js/projects/show.js
@@ -1,10 +1,8 @@
 (function() {
-    function projectBoard($scope, restFactory) {
+    function projectBoard($scope, $uibModal, restFactory) {
         $scope.statuses = ['Planning', 'In Progress', 'Resolved', 'Promoted', 'Closed'];
         $scope.issues = [];
-        $scope.project = { projectKey: '' };
-
-        
+        $scope.project = { projectKey: '' };  
 
         $scope.loadIssues = function() {
             return restFactory.get('/issues', { projectkey: $scope.project.key }).then(function(data) {
@@ -16,16 +14,39 @@
             });
         }
 
+        $scope.showIssue = function(issue) {
+            $scope.issue = issue;
+            var modalInstance = $uibModal.open({
+                templateUrl: 'modal.html',
+                controller: 'projectBoardModalController',
+                scope: $scope,
+                size: 'lg'
+            });
+
+            modalInstance.result.then(function() {}, function() {});
+        }
+
         restFactory.get(window.location.pathname).then(function(data) {
             $scope.project = data;
             $scope.$apply();
         }).then(function() {
             $scope.loadIssues();
-        });
-        
+        });       
     };
 
-    projectBoard.$inject = ['$scope', 'restFactory'];
+    projectBoard.$inject = ['$scope', '$uibModal', 'restFactory'];
 
-    angular.module('break').controller('projectBoardController', projectBoard);
+    function boardModalController($scope, $uibModalInstance) {
+        console.log($scope.issue);
+
+        $scope.close = function() {
+            $uibModalInstance.dismiss('cancel');
+        }
+    }
+
+    boardModalController.$inject = ['$scope', '$uibModalInstance'];
+
+    angular.module('break')
+    .controller('projectBoardController', projectBoard)
+    .controller('projectBoardModalController', boardModalController);
 })();
